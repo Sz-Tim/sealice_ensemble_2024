52886f0a78c33faed04498ce0966c8f4395bc110
Sz-Tim <tim.szewczyk@gmail.com>
2024-09-18 11:22:44 +0100

    EnsBlend splines

--- code/06a_ensemble_mixture.R
+++ code/06a_ensemble_mixture.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(glue)
 library(rstan)
+library(recipes)
 rstan_options(auto_write=T)
 
 source("code/00_fn.R")
@@ -11,7 +12,8 @@ source("code/00_fn.R")
 # ensembling --------------------------------------------------------------
 
 # Full dataset
-ensFull_df <- read_csv("out/valid_df.csv")
+ensFull_df <- read_csv("out/valid_df.csv") |>
+  mutate(across(starts_with("sim_"), ~.x - mean(.x), .names="c_{.col}"))
 years <- unique(ensFull_df$year)
 
 sim_i <- read_csv("out/sim_2019-2023/sim_i.csv") |>
@@ -32,48 +34,223 @@ sim_i <- read_csv("out/sim_2019-2023/sim_i.csv") |>
          lab_short=factor(lab_short, 
                           levels=c("Ens['Fc-1']", "Ens['Fc-5']", "Ens['M']", "Ens['Mix']", "Mean2D", "Mean3D", 
                                    "2D", "3D", "Null")))
+sims_3D <- filter(sim_i, lab_short=="3D")$sim
+sims_2D <- filter(sim_i, lab_short=="2D")$sim
+
+site_i <- read_csv("data/farm_sites.csv")
+ensFull_LatLon <- read_csv("out/valid_df.csv") |>
+  select(rowNum, date, sepaSite, sepaSiteNum, licePerFish_rtrt, starts_with("sim")) |>
+  select(-contains("avg")) |>
+  mutate(across(starts_with("sim_"), ~.x - mean(.x), .names="c_{.col}")) |>
+  left_join(site_i) |>
+  select(-sepaSite) |>
+  arrange(rowNum)
+
+
+ensMix_splineD4_rec_all <- recipe(licePerFish_rtrt ~ ., data=ensFull_LatLon) |>
+  step_interact(~easting:northing) |>
+  step_bs(easting, northing, easting_x_northing, deg_free=4) |>
+  update_role(c(rowNum, date, sepaSiteNum, starts_with("c_")), new_role="id") |>
+  update_role_requirements("id", bake=F) |>
+  prep()
+saveRDS(ensMix_splineD4_rec_all, "out/ensembles/recipe_sLonLatD4_all.rds")
+
+ensMix_splineD4_rec_3D <- recipe(licePerFish_rtrt ~ ., 
+                                 data=ensFull_LatLon |>
+                                   select(-any_of(sims_2D)) |>
+                                   select(-any_of(paste0("c_", sims_2D)))) |>
+  step_interact(~easting:northing) |>
+  step_bs(easting, northing, easting_x_northing, deg_free=4) |>
+  update_role(c(rowNum, date, sepaSiteNum, starts_with("c_")), new_role="id") |>
+  update_role_requirements("id", bake=F) |>
+  prep()
+saveRDS(ensMix_splineD4_rec_all, "out/ensembles/recipe_sLonLatD4_3D.rds")
+
+
+ensMix_splineD6_rec_all <- recipe(licePerFish_rtrt ~ ., data=ensFull_LatLon) |>
+  step_interact(~easting:northing) |>
+  step_bs(easting, northing, easting_x_northing, deg_free=6) |>
+  update_role(c(rowNum, date, sepaSiteNum, starts_with("c_")), new_role="id") |>
+  update_role_requirements("id", bake=F) |>
+  prep()
+saveRDS(ensMix_splineD6_rec_all, "out/ensembles/recipe_sLonLatD6_all.rds")
+
+ensMix_splineD6_rec_3D <- recipe(licePerFish_rtrt ~ ., 
+                                 data=ensFull_LatLon |>
+                                   select(-any_of(sims_2D)) |>
+                                   select(-any_of(paste0("c_", sims_2D)))) |>
+  step_interact(~easting:northing) |>
+  step_bs(easting, northing, easting_x_northing, deg_free=6) |>
+  update_role(c(rowNum, date, sepaSiteNum, starts_with("c_")), new_role="id") |>
+  update_role_requirements("id", bake=F) |>
+  prep()
+saveRDS(ensMix_splineD6_rec_all, "out/ensembles/recipe_sLonLatD6_3D.rds")
+
+ensMix_splineD8_rec_all <- recipe(licePerFish_rtrt ~ ., data=ensFull_LatLon) |>
+  step_interact(~easting:northing) |>
+  step_bs(easting, northing, easting_x_northing, deg_free=8) |>
+  update_role(c(rowNum, date, sepaSiteNum, starts_with("c_")), new_role="id") |>
+  update_role_requirements("id", bake=F) |>
+  prep()
+saveRDS(ensMix_splineD8_rec_all, "out/ensembles/recipe_sLonLatD8_all.rds")
+
+ensMix_splineD8_rec_3D <- recipe(licePerFish_rtrt ~ ., 
+                               data=ensFull_LatLon |>
+                                 select(-any_of(sims_2D)) |>
+                                 select(-any_of(paste0("c_", sims_2D)))) |>
+  step_interact(~easting:northing) |>
+  step_bs(easting, northing, easting_x_northing, deg_free=8) |>
+  update_role(c(rowNum, date, sepaSiteNum, starts_with("c_")), new_role="id") |>
+  update_role_requirements("id", bake=F) |>
+  prep()
+saveRDS(ensMix_splineD8_rec_all, "out/ensembles/recipe_sLonLatD8_3D.rds")
+
+
 
 
 
 
 # cross-validation --------------------------------------------------------
 
-mods <- expand_grid(mod=c("ranef"),
-                    d=c("3D"))
+mods <- expand_grid(mod=c("ranef", "sLonLatD4", "sLonLatD6", "sLonLatD8"),
+                    d=c("3D", "all"))
 
 CV_ensMix <- vector("list", length(years))
 
-for(yr in seq_along(years)) {
+for(yr in rev(seq_along(years))) {
   CV_yr <- vector("list", nrow(mods))
   for(i in 1:nrow(mods)) {
     mod <- mods$mod[i]
-    train_df <- ensFull_df |>
-      filter(year != years[yr]) |>
-      select(rowNum, sepaSite, sepaSiteNum, date, licePerFish_rtrt,
-             any_of(filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim), 
-             any_of(paste0("c_", filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim)))
-    test_df <- ensFull_df |>
-      filter(year == years[yr]) |>
-      select(rowNum, sepaSite, sepaSiteNum, date, licePerFish_rtrt, 
-             any_of(filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim), 
-             any_of(paste0("c_", filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim)))
-    dat_rstan <- make_data_rstan(train_df, R2D2_sd=F)
-    pars <- c("b_p", "b_b0", "b_IP", "b_hu", "sigma", "Intercept_hu",
-              "b_p_uc", "r_grp", "sd_grp")
+    if(grepl("sLonLat", mods$mod[i])) {
+      if(mods$d[i]=="3D") {
+        if(grepl("D4", mods$mod[i])) {
+          train_df <- ensMix_splineD4_rec_3D |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) != years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(sims_3D), 
+                          any_of(paste0("c_", sims_3D))))
+          test_df <- ensMix_splineD4_rec_3D |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) == years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(sims_3D), 
+                          any_of(paste0("c_", sims_3D))))
+        } else if(grepl("D6", mods$mod[i])) {
+          train_df <- ensMix_splineD6_rec_3D |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) != years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(sims_3D), 
+                          any_of(paste0("c_", sims_3D))))
+          test_df <- ensMix_splineD6_rec_3D |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) == years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(sims_3D), 
+                          any_of(paste0("c_", sims_3D))))
+        } else if(grepl("D8", mods$mod[i])) {
+          train_df <- ensMix_splineD8_rec_3D |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) != years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(sims_3D), 
+                          any_of(paste0("c_", sims_3D))))
+          test_df <- ensMix_splineD8_rec_3D |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) == years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(sims_3D), 
+                          any_of(paste0("c_", sims_3D)))) 
+        }
+      } else {
+        if(grepl("D4", mods$mod[i])) {
+          train_df <- ensMix_splineD4_rec_all |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) != years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(c(sims_3D, sims_2D)), 
+                          any_of(paste0("c_", c(sims_3D, sims_2D)))))
+          test_df <- ensMix_splineD4_rec_all |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) == years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(c(sims_3D, sims_2D)), 
+                          any_of(paste0("c_", c(sims_3D, sims_2D)))))
+        } else if(grepl("D6", mods$mod[i])) {
+          train_df <- ensMix_splineD6_rec_all |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) != years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(c(sims_3D, sims_2D)), 
+                          any_of(paste0("c_", c(sims_3D, sims_2D)))))
+          test_df <- ensMix_splineD6_rec_all |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) == years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(c(sims_3D, sims_2D)), 
+                          any_of(paste0("c_", c(sims_3D, sims_2D)))))
+        } else if(grepl("D8", mods$mod[i])) {
+          train_df <- ensMix_splineD8_rec_all |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) != years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(c(sims_3D, sims_2D)), 
+                          any_of(paste0("c_", c(sims_3D, sims_2D)))))
+          test_df <- ensMix_splineD8_rec_all |>
+            bake(ensFull_LatLon |> 
+                   filter(year(date) == years[yr]) |>
+                   select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                          any_of(c(sims_3D, sims_2D)), 
+                          any_of(paste0("c_", c(sims_3D, sims_2D)))))
+        }
+      }
+      dat_rstan <- make_data_rstan_sLonLat(train_df)
+      pars <- c("b_b0", "b_IP", "b_hu", "sigma", "Intercept_hu",
+                paste0("b_s_", c("easting", "northing", "easting_x_northing")))
+    } else {
+      train_df <- ensFull_df |>
+        filter(year != years[yr]) |>
+        select(rowNum, sepaSite, sepaSiteNum, date, licePerFish_rtrt,
+               any_of(filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim), 
+               any_of(paste0("c_", filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim)))
+      test_df <- ensFull_df |>
+        filter(year == years[yr]) |>
+        select(rowNum, sepaSite, sepaSiteNum, date, licePerFish_rtrt, 
+               any_of(filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim), 
+               any_of(paste0("c_", filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim)))
+      dat_rstan <- make_data_rstan(train_df, R2D2_sd=F)
+      pars <- c("b_p", "b_b0", "b_IP", "b_hu", "sigma", "Intercept_hu",
+                "b_p_uc", "r_grp", "sd_grp")
+    }
     if(grepl("pRE_huRE", mods$mod[i])) {
       pars <- c(pars, "r_grp_hu", "sd_grp_hu") 
     }
-    out_ensMix <- stan(file=glue("code/stan/ensemble_mixture_model_{mods$mod[i]}.stan"),
-                       model_name=mods$mod[i], data=dat_rstan,
-                       chains=3, cores=3, iter=2000, warmup=1500,
-                       control=list(adapt_delta=0.95, max_treedepth=20),
-                       pars=pars)
-    saveRDS(out_ensMix, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_CV-{years[yr]}_stanfit.rds"))
-    saveRDS(dat_rstan, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_CV-{years[yr]}_standata.rds"))
-    CV_yr[[i]] <- make_predictions_ensMix(out_ensMix, test_df, re=T, re_hu=F) |>
-      colMeans() |>
-      as_tibble() |>
-      set_names(paste0("IP_", mods$mod[i], "_", mods$d[i]))
+    if(file.exists(glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_CV-{years[yr]}_stanfit.rds"))) {
+      cat("File exists:", mods$d[i], mods$mod[i], years[yr], "\n")
+      out_ensMix <- readRDS(glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_CV-{years[yr]}_stanfit.rds"))
+    } else {
+      stanMod <- str_remove(mods$mod[i], "D[0-9]")
+      out_ensMix <- stan(file=glue("code/stan/ensemble_mixture_model_{stanMod}.stan"),
+                         model_name=mods$mod[i], data=dat_rstan,
+                         chains=30, cores=30, iter=2250, warmup=2000,
+                         # chains=1, cores=1, iter=22, warmup=20,
+                         control=list(adapt_delta=0.95, max_treedepth=20),
+                         pars=pars)
+      saveRDS(out_ensMix, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_CV-{years[yr]}_stanfit.rds"))
+      saveRDS(dat_rstan, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_CV-{years[yr]}_standata.rds")) 
+    }
+    if(grepl("sLonLat", mods$mod[i])) {
+      CV_yr[[i]] <- make_predictions_ensMix_sLonLat(out_ensMix, test_df, mode="epred", iter=10) |>
+        colMeans() |>
+        as_tibble() |>
+        set_names(paste0("IP_", mods$mod[i], "_", mods$d[i]))
+    } else {
+      CV_yr[[i]] <- make_predictions_ensMix(out_ensMix, test_df, re=T, re_hu=grepl("huRE", mods$mod[i])) |>
+        colMeans() |>
+        as_tibble() |>
+        set_names(paste0("IP_", mods$mod[i], "_", mods$d[i])) 
+    }
   }
   CV_ensMix[[yr]] <- bind_cols(test_df |> select(rowNum), reduce(CV_yr, bind_cols))
 }
@@ -85,26 +262,80 @@ reduce(CV_ensMix, bind_rows) |>
 
 # full dataset ------------------------------------------------------------
 
-mods <- expand_grid(mod=c("ranef"),
-                    d=c("3D"))
+mods <- expand_grid(mod=c("ranef", "sLonLatD4", "sLonLatD6", "sLonLatD8"),
+                    d=c("3D", "all")) |>
+  arrange(desc(mod), d)
 
 for(i in 1:nrow(mods)) {
   mod <- mods$mod[i]
-  train_df <- ensFull_df |>
-    select(rowNum, sepaSite, sepaSiteNum, date, licePerFish_rtrt,
-           any_of(filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim), 
-           any_of(paste0("c_", filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim)))
-  dat_rstan <- make_data_rstan(train_df, R2D2_sd=F)
-  pars <- c("b_p", "b_b0", "b_IP", "b_hu", "sigma", "Intercept_hu",
-            "b_p_uc", "r_grp", "sd_grp")
-  if(grepl("pRE_huRE", mods$mod[i])) {
-    pars <- c(pars, "r_grp_hu", "sd_grp_hu") 
+  if(grepl("sLonLat", mods$mod[i])) {
+    if(mods$d[i]=="3D") {
+      if(grepl("D4", mods$mod[i])) {
+        train_df <- ensMix_splineD4_rec_3D |>
+          bake(ensFull_LatLon |> 
+                 select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                        any_of(sims_3D), 
+                        any_of(paste0("c_", sims_3D))))
+      } else if(grepl("D6", mods$mod[i])) {
+        train_df <- ensMix_splineD6_rec_3D |>
+          bake(ensFull_LatLon |> 
+                 select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                        any_of(sims_3D), 
+                        any_of(paste0("c_", sims_3D))))
+      } else if(grepl("D8", mods$mod[i])){
+        train_df <- ensMix_splineD8_rec_3D |>
+          bake(ensFull_LatLon |> 
+                 select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                        any_of(sims_3D), 
+                        any_of(paste0("c_", sims_3D))))
+      }
+    } else {
+      if(grepl("D4", mods$mod[i])) {
+        train_df <- ensMix_splineD4_rec_all |>
+          bake(ensFull_LatLon |> 
+                 select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                        any_of(c(sims_3D, sims_2D)), 
+                        any_of(paste0("c_", c(sims_3D, sims_2D)))))
+      } else if(grepl("D6", mods$mod[i])) {
+        train_df <- ensMix_splineD6_rec_all |>
+          bake(ensFull_LatLon |> 
+                 select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                        any_of(c(sims_3D, sims_2D)), 
+                        any_of(paste0("c_", c(sims_3D, sims_2D)))))
+      } else if(grepl("D8", mods$mod[i])) {
+        train_df <- ensMix_splineD8_rec_all |>
+          bake(ensFull_LatLon |> 
+                 select(rowNum, easting, northing, sepaSiteNum, date, licePerFish_rtrt,
+                        any_of(c(sims_3D, sims_2D)), 
+                        any_of(paste0("c_", c(sims_3D, sims_2D)))))
+      }
+    }
+    dat_rstan <- make_data_rstan_sLonLat(train_df)
+    pars <- c("b_b0", "b_IP", "b_hu", "sigma", "Intercept_hu",
+              paste0("b_s_", c("easting", "northing", "easting_x_northing")))
+  } else {
+    train_df <- ensFull_df |>
+      select(rowNum, sepaSite, sepaSiteNum, date, licePerFish_rtrt,
+             any_of(filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim),
+             any_of(paste0("c_", filter(sim_i, lab_short %in% c("3D", ifelse(mods$d[i]=="all", "2D", "")))$sim)))
+    dat_rstan <- make_data_rstan(train_df, R2D2_sd=F)
+    pars <- c("b_p", "b_b0", "b_IP", "b_hu", "sigma", "Intercept_hu",
+              "b_p_uc", "r_grp", "sd_grp")
+    if(grepl("pRE_huRE", mods$mod[i])) {
+      pars <- c(pars, "r_grp_hu", "sd_grp_hu") 
+    } 
   }
-  out_ensMix <- stan(file=glue("code/stan/ensemble_mixture_model_{mods$mod[i]}.stan"),
-                     model_name=mods$mod[i], data=dat_rstan,
-                     chains=3, cores=3, iter=4000, warmup=3000,
-                     control=list(adapt_delta=0.95, max_treedepth=20),
-                     pars=pars)
-  saveRDS(out_ensMix, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_FULL_stanfit.rds"))
-  saveRDS(dat_rstan, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_FULL_standata.rds"))
+  if(!file.exists(glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_FULL_stanfit.rds"))) {
+    stanMod <- str_remove(mods$mod[i], "D[0-9]")
+    out_ensMix <- stan(file=glue("code/stan/ensemble_mixture_model_{stanMod}.stan"),
+                       model_name=mods$mod[i], data=dat_rstan,
+                       chains=30, cores=15, iter=2250, warmup=2000,
+                       control=list(adapt_delta=0.95, max_treedepth=20),
+                       pars=pars)
+    saveRDS(out_ensMix, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_FULL_stanfit.rds"))
+    saveRDS(dat_rstan, glue("out/ensembles/ensMix_{mods$d[i]}_{mods$mod[i]}_FULL_standata.rds"))
+  } else {
+    cat("File exists:", mods$d[i], mods$mod[i], "\n")
+    Sys.sleep(3)
+  }
 }
--- code/stan/ensemble_mixture_model_sLonLat.stan
+++ code/stan/ensemble_mixture_model_sLonLat.stan
@@ -17,7 +17,6 @@ functions {
              lognormal_lpdf(y | mu, sigma);
     }
   }
-  
   /* hurdle lognormal log-PDF of a single response
    * logit parameterization of the hurdle part
    * Args:
@@ -36,7 +35,6 @@ functions {
              lognormal_lpdf(y | mu, sigma);
     }
   }
-  
   // hurdle lognormal log-CCDF and log-CDF functions
   real hurdle_lognormal_lccdf(real y, real mu, real sigma, real hu) {
     return bernoulli_lpmf(0 | hu) + lognormal_lccdf(y | mu, sigma);
@@ -44,7 +42,6 @@ functions {
   real hurdle_lognormal_lcdf(real y, real mu, real sigma, real hu) {
     return log1m_exp(hurdle_lognormal_lccdf(y | mu, sigma, hu));
   }
-  
   /* compute scale parameters of the R2D2 prior
    * Args:
    *   phi: local weight parameters
@@ -55,6 +52,15 @@ functions {
   vector scales_R2D2(vector phi, real tau2) {
     return sqrt(phi * tau2);
   }
+  /* softmax for a row vector
+   * Args:
+   *   y: (unconstrained) row vector
+   * Returns:
+   *   row vector simplex
+   */
+   row_vector softmax_row_vector(row_vector y) {
+     return exp(y) / sum(exp(y));
+   }
 }
 data {
   int<lower=1> N;  // total number of observations
@@ -65,101 +71,97 @@ data {
   int<lower=0> N_Yg0; // Number of y > 0 's
   array[N_Yg0] int indexes_Yg0; // Indexes of Y where y > 0
   int<lower=1> K_sims;  // number of candidate models
-  matrix[N, K_sims] X_p;  // design matrix
-  
-  // data for spline 1 
-  int nb_pMix02_1;  // number of bases = K_sims?
-  array[nb_pMix02_1] int knots_pMix02_1;  // number of knots
-  // basis function matrices // matrices created by mgcv::smoothCon I think
-  matrix[N, knots_pMix02_1[1]] Zs_pMix02_1_1;
-  
+  matrix[N, K_sims] X;  // design matrix
+  matrix[N, K_sims] Xc; // design matrix (centered)
+  // // data for group-level effects 
+  int<lower=1> N_groups;  // number of grouping levels (sites)
+  array[N] int<lower=1> J_group;  // grouping indicator per observation
+  // data for splines
+  int<lower=1> K_knots; // including intercept
+  matrix[N_groups, K_knots] Xs_easting;
+  matrix[N_groups, K_knots] Xs_northing;
+  matrix[N_groups, K_knots] Xs_easting_x_northing;
   int prior_only;  // should the likelihood be ignored?
+  // R2D2 priors
+  real<lower=0> R2D2_mean_R2_hu;  // mean of the R2 prior
+  real<lower=0> R2D2_prec_R2_hu;  // precision of the R2 prior
+  vector<lower=0>[K_sims] R2D2_cons_D2_hu; // concentration vector of the D2 prior
 }
 transformed data {
   // 1's and 0's to feed to bernoulli_lpmf; note y=0 --> bern(1|hu)
   array[N] int bern_01;
   bern_01[indexes_Ye0] = ones_int_array(N_Ye0);
   bern_01[indexes_Yg0] = zeros_int_array(N_Yg0);
-  matrix[N_Yg0, K_sims] X_p_Yg0 = X_p[indexes_Yg0,];
+  matrix[N_Yg0, K_sims] X_Yg0 = X[indexes_Yg0,];
   vector[N_Yg0] Y_Yg0 = Y[indexes_Yg0];
+  array[N_Yg0] int<lower=1> J_group_Yg0 = J_group[indexes_Yg0];  // grouping indicator per observation
   int<lower=1> K_int_sims = K_sims + 1;  // number of candidate models + 1 (intercept)
   int<lower=1> Kscales_hu = K_sims;
-  matrix[N, K_int_sims] X_hu = append_col(rep_vector(1.0, N), X_p);
-  matrix[N, K_sims] Xc_hu;  // centered version of X_hu without an intercept
-  vector[K_sims] means_X_hu;  // column means of X_hu before centering
-  for (i in 2:K_int_sims) {
-    means_X_hu[i - 1] = mean(X_hu[, i]);
-    Xc_hu[, i - 1] = X_hu[, i] - means_X_hu[i - 1];
+  matrix[N, K_int_sims] XInt = append_col(ones_vector(N), X);
+  vector[K_sims] means_X;  // column means of X before centering
+  for (i in 1:K_sims) {
+    means_X[i] = mean(X[,i]);
   }
-  // concentration vector of the D2 prior
-  vector<lower=0>[Kscales_hu] R2D2_cons_D2_hu = rep_vector(0.5, Kscales_hu);
+  matrix[N, K_int_sims] XcInt = append_col(ones_vector(N), Xc);  // centered version of X with an intercept
 }
 parameters {
-  simplex[K_sims] b_p;  // mixture proportions
   real b_b0;  // regression coefficient
   real<lower=0,upper=1> b_IP;  // regression coefficient
-  
-  vector<lower=0,upper=1>[K_sims] bs_pMix02;  // unpenalized spline coefficients
-  // parameters for spline 1
-  // standardized penalized spline coefficients
-  vector[knots_pMix02_1[1]] zs_pMix02_1_1;
-  vector<lower=0>[nb_pMix02_1] sds_pMix02_1;  // SDs of penalized spline coefficients
-  
   real<lower=0> sigma;  // dispersion parameter
   vector[K_sims] zb_hu;  // unscaled hurdle coefficients
   real Intercept_hu;  // temporary intercept for centered predictors
+  matrix[K_knots,K_sims] b_s_easting;
+  matrix[K_knots,K_sims] b_s_northing;
+  matrix[K_knots,K_sims] b_s_easting_x_northing;
   // parameters of the R2D2 prior
   real<lower=0,upper=1> R2D2_R2_hu;
   simplex[Kscales_hu] R2D2_phi_hu;
-  
 }
 transformed parameters {
+  matrix[N_groups,K_sims] b_p_uc;  // mixture proportions (unconstrained)
+  matrix[N_groups,K_sims] b_p;  // mixture proportions (unconstrained)
   vector[K_sims] b_hu;  // scaled coefficients
-  
-  // penalized spline coefficients
-  vector[knots_pMix02_1[1]] s_pMix02_1_1;
-  
   vector<lower=0>[K_sims] sdb_hu;  // SDs of the coefficients
   real R2D2_tau2_hu;  // global R2D2 scale parameter
   vector<lower=0>[Kscales_hu] scales_hu;  // local R2D2 scale parameters
   real lprior = 0;  // prior contributions to the log posterior
-  
-  // compute penalized spline coefficients
-  s_pMix02_1_1 = sds_pMix02_1[1] * zs_pMix02_1_1;
-  
+  // compute unconstrained b_p_uc based on lon-lat splines
+  for(k in 1:K_sims) {
+    b_p_uc[,k] = Xs_easting * b_s_easting[,k] + 
+      Xs_northing * b_s_northing[,k] +
+      Xs_easting_x_northing * b_s_easting_x_northing[,k];
+  }
+  for(j in 1:N_groups) {
+    b_p[j,] = softmax_row_vector(b_p_uc[j,]);
+  }
   // compute R2D2 scale parameters
   R2D2_tau2_hu = R2D2_R2_hu / (1 - R2D2_R2_hu);
   scales_hu = scales_R2D2(R2D2_phi_hu, R2D2_tau2_hu);
   sdb_hu = scales_hu[1:K_sims];
   b_hu = zb_hu .* sdb_hu;  // scale coefficients
-  
   // priors
-  lprior += normal_lpdf(b_p | 0.5, 2)
-    - 1 * log_diff_exp(normal_lcdf(1 | 0.5, 2), normal_lcdf(0 | 0.5, 2));
   lprior += normal_lpdf(b_b0 | 0, 1);
   lprior += normal_lpdf(b_IP | 0.25, 1)
     - 1 * log_diff_exp(normal_lcdf(1 | 0.25, 1), normal_lcdf(0 | 0.25, 1));
-  
-  lprior += normal_lpdf(bs_pMix02 | 0.5, 1)
-    - 2 * log_diff_exp(normal_lcdf(1 | 0.5, 1), normal_lcdf(0 | 0.5, 1));
-  lprior += student_t_lpdf(sds_pMix02_1 | 3, 0, 2.5)
-    - 1 * student_t_lccdf(0 | 3, 0, 2.5);
-  
   lprior += student_t_lpdf(sigma | 3, 0, 2.5)
     - 1 * student_t_lccdf(0 | 3, 0, 2.5);
   lprior += logistic_lpdf(Intercept_hu | 0, 1);
-  lprior += beta_lpdf(R2D2_R2_hu | 0.5 * 2, (1 - 0.5) * 2);
+  for(i in 1:K_sims) {
+    lprior += normal_lpdf(b_s_easting[,i] | 0, 1);
+    lprior += normal_lpdf(b_s_northing[,i] | 0, 1);
+    lprior += normal_lpdf(b_s_easting_x_northing[,i] | 0, 1);
+  }
+  lprior += beta_lpdf(R2D2_R2_hu | R2D2_mean_R2_hu * R2D2_prec_R2_hu, (1 - R2D2_mean_R2_hu) * R2D2_prec_R2_hu);
 }
 model {
   // likelihood including constants
   if (!prior_only) {
     vector[N_Yg0] mu;
+    vector[N_Yg0] IP_ens;
     vector[N] hu;
-    
-    nlp_pMix02 += X_pMix02 * b_pMix02 + X_p_Yg0 * bs_pMix02 + Zs_pMix02_1_1 * s_pMix02_1_1;
-    
-    hu = Intercept_hu + Xc_hu * b_hu;
-    mu = b_b0 + b_IP * (X_p_Yg0 * b_p);
+    hu = Intercept_hu + Xc * b_hu;
+    IP_ens = rows_dot_product(b_p[J_group_Yg0,], X_Yg0);
+    mu = b_b0 + b_IP * IP_ens;
     target += bernoulli_logit_lpmf(bern_01 | hu);
     target += lognormal_lpdf(Y_Yg0 | mu, sigma);
   }
@@ -168,8 +170,9 @@ model {
   target += std_normal_lpdf(zb_hu);
   target += dirichlet_lpdf(R2D2_phi_hu | R2D2_cons_D2_hu);
 }
+
 generated quantities {
   // actual population-level intercept
-  real b_hu_Intercept = Intercept_hu - dot_product(means_X_hu, b_hu);
+  real b_hu_Intercept = Intercept_hu - dot_product(means_X, b_hu);
 }
 
